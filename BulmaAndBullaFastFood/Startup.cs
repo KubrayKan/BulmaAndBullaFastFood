c40b652b396a274a4970f6bd42a581f9deef16da
KubrayKan <guillermo.tremols@gmail.com>
2020-10-06 20:06:00 -0400

    bruh

---
+++ BulmaAndBullaFastFood/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BulmaAndBullaFastFood.Startup))]
+namespace BulmaAndBullaFastFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //bruh
+            ConfigureAuth(app);
+        }
+    }
+}
